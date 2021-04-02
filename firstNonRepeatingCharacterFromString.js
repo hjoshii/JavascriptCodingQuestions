a9f48db5ba3641df41766907b784d2c392249a67
hjoshii <48127016+hjoshii@users.noreply.github.com>
2021-04-02 10:37:23 +0530

    Add files via upload

---
+++ firstNonRepeatingCharacterFromString.js
@@ -0,0 +1,12 @@
+function firstNonRepeatingCharacter(str) {
+  for (let i = 0; i < str.length; i++) {
+    let char = str[i];
+    if (str.indexOf(char) == i && str.indexOf(char, i + 1) == -1) {
+      return char;
+    }
+  }
+  return "_";
+}
+
+firstNonRepeatingCharacter("kakmal")
+
